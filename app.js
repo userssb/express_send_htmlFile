284489a369f0aa621a79eb738ffecb53e766bb5d
userssb <sreeramoju.sreekanthbabu@gmail.com>
2022-03-23 13:13:56 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+app.listen(3000);
